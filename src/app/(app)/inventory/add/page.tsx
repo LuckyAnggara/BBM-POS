c65e0234e61d6513718f9407f4b79f3382a5f9a9
Lucky Anggara <Anggara.lucky1992@gmail.com>
2025-06-06 09:05:06 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

---
+++ src/app/(app)/inventory/add/page.tsx
@@ -0,0 +1,6 @@
+// This file is a placeholder.
+// The actual route for /inventory/add is handled by /src/app/inventory/add/page.tsx
+// You can delete this file if /src/app/inventory/add/page.tsx is the intended active route.
+export default function PlaceholderInventoryAddPageInAppGroup() {
+  return null;
+}
